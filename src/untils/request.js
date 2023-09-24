a26ccc1dc7d1f1ba4c2f47cc4eee195098538ea6
Nghiem Tuan Anh <nghiemtuananh2004@gmail.com>
2023-09-24 13:11:54 +0700

    fix http

--- src/untils/request.js
+++ src/untils/request.js
@@ -1,4 +1,4 @@
-const API_DOMAIN = 'https://localhost:3000/';
+const API_DOMAIN = 'http://localhost:3000/';
 
 export const get = async (path) => {
     const response = await fetch(API_DOMAIN + path);
